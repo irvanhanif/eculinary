e5a2a60ba0d5e153e66443fd46ad10df33cc17a8
ellenwidyaa <74695281+ellenwidyaa@users.noreply.github.com>
2022-05-13 19:26:37 +0700

    Add files via upload

---
+++ view/menu/makanan.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="makanan-wrapper">
+        <div class="makanan">
+            <img src="assets/mie_gacoan.png" alt="foto-makanan">
+            <h4><b>Mie Iblis</b></h4>
+            <div class="informasi-makanan">
+                <h4>Rp 10.000</h4>
+                <div class="komentar">
+                    <img src="assets/chat.png" alt="chat">
+                    <h5>1rb</h5>
+                </div>
+            </div>
+            <div class="informasi-makanan">
+                <h4 style="color:#8b8b8b">Malang</h4>
+                <div class="rating">
+                    <img src="assets/star.png" alt="star">
+                </div>
+            </div>
+        </div>
+        </div>
+    </body>
+</html>
