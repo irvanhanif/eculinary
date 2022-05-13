ff87175b29877efd7bc233f6bcaf41ff1ec2e8b2
ellenwidyaa <74695281+ellenwidyaa@users.noreply.github.com>
2022-05-13 21:40:04 +0700

    Update homepage.php

--- view/homepage.php
+++ view/homepage.php
@@ -3,102 +3,113 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="view/css/homepage.css">
-<?php require_once 'header.html'; ?>
-<img src="view/asset/banner.png" alt="banner">
-<p>Kategori</p>
+    <link rel="stylesheet" href="homepage.css">
 
-<div class="cattegories">
-<img src="view/asset/ayam.png" alt="ayam">
-<p>Ayam</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/nasgor.png" alt="nasgor">
-<p>Nasi Goreng</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/sup.png" alt="sup">
-<p>Sup</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/mie.png" alt="mie">
-<p>Mie</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/bakso.png" alt="bakso">
-<p>Bakso</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/pizza.png" alt="pizza">
-<p>Pizza</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/sup.png" alt="sup">
-<p>Sup</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/burger.png" alt="burger">
-<p>Burger</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/kopi.png" alt="kopi">
-<p>Kopi</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/jus.png" alt="jus">
-<p>Jus</p>
-</div>
-<div class="cattegories">
-<img src="view/asset/eskrim.png" alt="eskrim">
-<p>Es Krim</p>
-</div>
-
-<p>Makanan & Minuman Populer</p>
-<div class="makanan">
-    <img src="view/asset/makanan.png" alt="makanan">
-    <img src="view/asset/wish.png" alt="wish">
-    <p>Mie Iblis</p>
-    <p>Rp 10.000</p>
-    <p>1rb</p>
-    <img src="view/asset/chat.png" alt="komentar">
-    <p>Malang</p>
-    <img src="view/asset/bintang.png" alt="rating">
-</div>
-
-<p>Rekomendasi Makanan & Minuman</p>
-<div class="rekomen">
-<img src="view/asset/makanan.png" alt="makanan">
-    <img src="view/asset/wish.png" alt="wish">
-    <p>Mie Iblis</p>
-    <p>Rp 10.000</p>
-    <p>1rb</p>
-    <img src="view/asset/chat.png" alt="komentar">
-    <p>Malang</p>
-    <img src="view/asset/bintang.png" alt="rating">
-</div>
-
-<p>Makanan & Minuman</p>
-<div class="makanminum">
-<img src="view/asset/makanan.png" alt="makanan">
-    <img src="view/asset/wish.png" alt="wish">
-    <p>Mie Iblis</p>
-    <p>Rp 10.000</p>
-    <p>1rb</p>
-    <img src="view/asset/chat.png" alt="komentar">
-    <p>Malang</p>
-    <img src="view/asset/bintang.png" alt="rating">
-</div>
-
-<p>Artikel</p>
-<div class="artikel">
-<img src="view/asset/makanan.png" alt="makanan">
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-<p>Isi Artikel</p>
-</div>
+<img class="banner" src="banner.png" alt="banner">
 
 
+<h2>Kategori</h2>
 
+<div class="kategori-wrapper">
 
+<div class="kategori">
 
-<?php require_once 'footer.html'; ?>
+<div class="cattegories">
+<a href="toko.php">
+<img class="gambar" alt="ayam" src="ayam.png">
+<p class="tulisan" href="">Ayam</p>
+</p>
+</div>
+
+<div class="cattegories">
+<a href= >
+         <img class="gambar" alt="nasgor" src="nasgor.png">
+         <p class="tulisan" href="">Nasi Goreng</p>
+      </a> 
+</div>
+
+<div class="cattegories">
+<a href= >
+         <img class="gambar" alt="sup" src="sup.png">
+         <p class="tulisan" href="">Sup</p>
+      </a>
+</div>
+
+<div class="cattegories">
+<a href= >
+         <img class="gambar" alt="mie" src="mie.png">
+         <p class="tulisan" href="">Mie</p>
+      </a>
+</div>
+
+<div class="cattegories">
+<a href= >
+         <img class="gambar" alt="bakso" src="bakso.png">
+         <p class="tulisan" href="">Bakso</p>
+      </a>
+</div>
+
+<div class="cattegories">
+<a href= >
+         <img class="gambar" alt="pizza" src="pizza.png">
+         <p class="tulisan" href="">Pizza</p>
+      </a>
+</div>
+
+<div class="cattegories">
+<a href= >
+         <img class="gambar" alt="burger" src="burger.png">
+         <p class="tulisan" href="">Burger</p>
+        </a>
+</div>
+
+<div class="cattegories">
+<a href= >
+         <img class="gambar" alt="kopi" src="kopi.png">
+         <p class="tulisan" href="">Kopi</p>
+      </a>
+</div>
+
+<div class="cattegories">
+<a href= >
+         <img class="gambar" alt="jus" src="jus.png">
+         <p class="tulisan" href="">Jus</p>
+      </a>
+</div>
+
+<div class="cattegories">
+<a href= >
+         <img class="gambar" alt="eskrim" src="eskrim.png">
+         <p class="tulisan" href="">Es Krim</p>
+</a>
+</div>
+</div>
+
+<div class="menu-wrapper">
+<h2>Makanan & Minuman Populer</h2>
+<?php 
+for ($i = 0; $i <= 3; $i++) {
+                require 'makanan.php';
+            }
+            ?>
+</div>
+
+<div class="menu-wrapper">
+<h2>Rekomendasi Makanan & Minuman</h2>
+<?php 
+for ($i = 0; $i <= 3; $i++) {
+                require 'makanan.php';
+            }
+            ?>  
+</div>
+
+<div class="menu-wrapper">
+<h2>Artikel</h2>
+<?php 
+for ($i = 0; $i <= 3; $i++) {
+                require 'makanan.php';
+            }
+            ?>  
+</div>
+
 </html>
