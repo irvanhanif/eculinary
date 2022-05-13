6b6a0b706e04dd331d65db33fc45ba9971f534b0
ellenwidyaa <74695281+ellenwidyaa@users.noreply.github.com>
2022-05-13 19:28:38 +0700

    Update detailtoko.php

--- view/toko/detailtoko.php
+++ view/toko/detailtoko.php
@@ -1,38 +1,46 @@
 <!DOCTYPE html>
-<html>
-<meta charset="UTF-8">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../view/css/homepage.css">
-<?php require_once './view/header.html'; ?>
-<img src="../view/asset/shop.png" alt="shop">
-    <p>Mie Gacoan Malang</p>
-    <p>Kota Malang</p>
-    <p>Ruko Kendalsari Barat, Jl. Kendal Sari Bar. No.2</p>
-    <img src="../view/asset/clipboard.png" alt="menu">
-    <p>Menu : </p>
-    <p>Urutan Berdasarkan</p>
-    <p>Terbaru</p>
-    <p>Populer</p>
-    <label for="harga">Harga</label>
-<select id="Harga" name="Harga">
-  <option value="rendahketinggi">Harga : Rendah ke Tinggi</option>
-  <option value="rendahketinggi">Harga : Tinggi ke Rendah</option>
-</select>
-<?php 
-    
-    for($i = 0; i < count($data); $i++){
-?>
-<div class="makanan">
-    <img src="../view/asset/makanan.png" alt="makanan">
-    <img src="../view/asset/wish.png" alt="wish">
-    <p>Mie Iblis</p>
-    <p>Rp 10.000</p>
-    <p>1rb</p>
-    <img src="../view/asset/chat.png" alt="komentar">
-    <p>Malang</p>
-    <img src="../view/asset/bintang.png" alt="rating">
-</div>
-<?php } ?>
-<?php require_once './view/footer.html'; ?>
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="content-wrapper">
+        <div class="detail-toko">
+            <div class="logo">
+                <img src="assets/logo_toko.png" alt="foto-toko">
+            </div>
+            <div class="informasi">
+                <h1>Mie Gacoan Malang</h1>
+                <h2>Kota Malang</h2>
+                <h3>Ruko Kendalsari Barat, Jl. Kendal Sari Bar. No.2</h3>
+                <div class="total-menu">
+                    <img src="assets/clipboard.png" alt="clipboard">
+                    <h2>Menu : <span>20</span></h2>
+                </div>
+            </div>
+        </div>
+        <div class="filter-wrapper">
+            <h2>Urutkan Berdasarkan</h2>
+            <form action="">
+                <input type="button" name="terbaru" value="Terbaru">
+                <input type="button" name="populer" value="Populer">
+                <select name="harga">
+                    <option value disabled selected>Harga</option>
+                    <option value="rendah">Harga : Rendah ke Tinggi</option>
+                    <option value="tinggi">Harga : Tinggi ke Rendah</option>
+                </select>
+            </form>
+        </div>
+        <?php 
+            for ($i = 0; $i <= 10; $i++) {
+                require 'makanan.php';
+            }
+        ?>
+    </div>
+    <footer></footer>
+</body>
 </html>
