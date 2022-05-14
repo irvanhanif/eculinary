e15af55f91b51c9b36e5d2079164ba095eba69ac
ellenwidyaa <74695281+ellenwidyaa@users.noreply.github.com>
2022-05-14 21:55:09 +0700

    Update detailmakanan.php

--- view/menu/detailmakanan.php
+++ view/menu/detailmakanan.php
@@ -3,19 +3,40 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="homepage.css">
-<img src="detailmakanan.png" alt="detailmakanan">
+    <link rel="stylesheet" href="detailmakanan.css">
+    <body>
+        <div class="makanan-wrapper">
+        <div class="makanan">
+        <img class="gambar"src="detailmakanan.png" alt="detailmakanan">
     <p>Es Campur Dempo</p>
     <p>Malang</p>
     <img src="bintang.png" alt="rating">
     <p>Rp 10.000</p>
+        </div class="text">
     <p>Silakan berikan bintang!</p>
     <img src="bintang.png" alt="rating">
-    <img src="clipboard.png" alt="menu">
+    </div>
+
     <p>Komentar</p>
+
+
     <img src="profile.png" alt="profil">
     <input type="text" name="komentar" placeholder="Tambahkan komentar...">
     <img src="plus.png" alt="tambahkan">
+
+    <div class="komentar1"><img src="profil1.png" alt="profil">
+    <p>Enak banget, pasti bakal kesinii lagii</p></div>
+
+    <div class="komentar2"><img src="profil1.png" alt="profil">
+        <p>Esnya Segerrr</p></div>
+
+    <div class="komentar3"><img src="profil1.png" alt="profil">
+        <p>Enak banget, pasti bakal kesinii lagii</p></div>
+
+    <div class="komentar4"><img src="profil1.png" alt="profil">
+        <p>Esnya Segerrr</p></div>
+
+    
     <div>
         <p>Kategori</p>
         <p>Minuman</p>
@@ -25,7 +46,10 @@
         <p>08.00-21.00</p>
         <p>Sabtu-Minggu</p>
         <p>10.00-23.00</p>
-        <p>Lihat Detail Toko</p>
-    </div>
-<?php require_once 'footer.html'; ?>
+
+<input type="button" value="Lihat Detail Toko"
+onclick="window.open('detailtoko.php')">
+
+    </div> 
+    </body>
 </html>
