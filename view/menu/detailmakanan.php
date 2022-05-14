0ae2c896480e9ff44d8cb413471cf8d61abf7023
ellenwidyaa <74695281+ellenwidyaa@users.noreply.github.com>
2022-05-15 01:31:32 +0700

    fixing detailMakanan

--- view/menu/detailmakanan.php
+++ view/menu/detailmakanan.php
@@ -3,53 +3,69 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="detailmakanan.css">
+    <link rel="stylesheet" href="../css/detailmakanan.css">
     <body>
+
         <div class="makanan-wrapper">
         <div class="makanan">
+            <div class="menu-wrapper">  
         <img class="gambar"src="../asset/detailmakanan.png" alt="detailmakanan">
-    <h2>Es Campur Dempo</h2>
-    <p>Malang</p>
+        <div class="text">
+        <p>Silakan berikan bintang!</p>
     <img src="../asset/bintang.png" alt="rating">
-    <p>Rp 10.000</p>
-        </div class="text">
-    <p>Silakan berikan bintang!</p>
-    <img src="../asset/bintang.png" alt="rating">
+        </div>
+        </div>
+        <div class="detail-wrapper">
+        <div class="menu">
+    <p class="detail"><b>Es Campur Dempo</b></p>
+    <p class="detail">Malang</p>
+    </div>
+    <div class="ratingHarga">
+    <img class="rating"src="../asset/bintang.png" alt="rating">
+    <p class="harga">Rp 10.000</p>
+    </div>
+        </div>
+        </div>
     </div>
 
-    <p>Komentar</p>
+    <div class="komentar">
+        <p class="komen">Komentar</p>
+        <div class="isi">
+        <img class="profil"src="../asset/profile.png" alt="profil">
+    <input class="kolom" type="text" name="komentar" placeholder="Tambahkan komentar...">
+    <form class="button-submit" action="">
+                <input type="button" name="submit">
+            </form>
+        </div>
 
+    <div class="comment"><img class="akun"src="../asset/profil1.png" alt="profil">
+    <p class="komentar1">Enak banget, pasti bakal kesinii lagii</p></div>
 
-    <img src="../asset/profile.png" alt="profil">
-    <input type="text" name="komentar" placeholder="Tambahkan komentar...">
-    <img src="../asset/plus.png" alt="tambahkan">
+    <div class="comment"><img class="akun" src="../asset/profil1.png" alt="profil">
+        <p class="komentar1">Esnya Segerrr</p></div>
 
-    <div class="komentar1"><img src="../asset/profil1.png" alt="profil">
-    <p>Enak banget, pasti bakal kesinii lagii</p></div>
+    <div class="comment"><img class="akun" src="../asset/profil1.png" alt="profil">
+        <p class="komentar1">Enak banget, pasti bakal kesinii lagii</p></div>
 
-    <div class="komentar2"><img src="../asset/profil1.png" alt="profil">
-        <p>Esnya Segerrr</p></div>
-
-    <div class="komentar3"><img src="../asset/profil1.png" alt="profil">
-        <p>Enak banget, pasti bakal kesinii lagii</p></div>
-
-    <div class="komentar4"><img src="../asset/profil1.png" alt="profil">
-        <p>Esnya Segerrr</p></div>
-
-    
+    <div class="comment"><img class="akun" src="../asset/profil1.png" alt="profil">
+        <p class="komentar1">Esnya Segerrr</p></div>
+        <a href="">
+         <img class="lanjut"alt="arrow-down" src="../asset/arrow-down.png"
+         width=30>
+        </div>
     <div>
-        <p>Kategori</p>
-        <p>Minuman</p>
-        <p>Es Krim</p>
-        <p>Jam Operasional</p>
-        <p>Senin-Jumat</p>
-        <p>08.00-21.00</p>
-        <p>Sabtu-Minggu</p>
-        <p>10.00-23.00</p>
 
+        <div class="tersedia">
+        <p class="judul">Kategori</p>
+        <p class="konten">Minuman</p>
+        <p class="konten">Es Krim</p>
+        <p class="judul">Jam Operasional</p>
+        <p class="hari">Senin-Jumat</p>
+        <p class="jam">08.00-21.00</p>
+        <p class="hari">Sabtu-Minggu</p>
+        <p class="jam">10.00-23.00</p>
 <input type="button" value="Lihat Detail Toko"
 onclick="window.open('detailtoko.php')">
-
-    </div> 
+        </div> 
     </body>
 </html>
