3e2fdb79110ed25bdb2b77c4ac944d564f0305b2
irvanhanif24 <irhan8803@gmail.com>
2022-05-14 15:25:23 +0700

    update register

--- view/user/register.php
+++ view/user/register.php
@@ -7,31 +7,29 @@
     <link rel="stylesheet" href="../view/css/register.css">
 </head>
 <?php require_once './view/header.html'; ?>
-    <form method="post" action="" method="post">
-<p class="daftar"><b>Daftar</b></p>
-<p>Username</p>
-<input class="kolom" type="text" name="username" placeholder="Masukan Username"
-required="required"><br>
-<p>Email</p>
-<input class="kolom" type="text" name="email" placeholder="Masukan Email"
-required="required"><br>
-<p>Kata Sandi</p>
-<input class="kolom" type="password" name="password" placeholder="Masukan Kata Sandi"
-required="required"><br>
-          <input type="checkbox" class="checkbox" required>
-            <p class="kebijakan">Saya menerima syarat dan ketentuan yang berlaku
-          </p>
-        <input class="submit" type="submit" value="Daftar" name="daftar">
-        </input>
-        <div class="kembali">
-            <p class="back">Kembali</p>
-        </div>
-          <p class="punya">Sudah mempunyai akun? <span style="color : #FFAB65" > Masuk</span></p>
-        </div>
-      </form>
-    <footer>
-        <p class="footer">© E-Culinary 2022. Hak Cipta Dilindungi</p>
-    </footer>
+<body>
+  <form method="post" action="" method="post">
+    <p class="daftar"><b>Daftar</b></p>
+    <p>Username</p>
+    <input class="kolom" type="text" name="username" placeholder="Masukan Username"required="required"><br>
+    <p>Email</p>
+    <input class="kolom" type="text" name="email" placeholder="Masukan Email" required="required"><br>
+    <p>Kata Sandi</p>
+    <input class="kolom" type="password" name="password" placeholder="Masukan Kata Sandi" required="required"><br>
+    <div class="kebijakan-box">
+      <input type="checkbox" class="checkbox" required>
+      <p class="kebijakan">Saya menerima syarat dan ketentuan yang berlaku</p>
+    </div>
+    <input class="submit" type="submit" value="Daftar" name="daftar">
+    <div class="kembali">
+      <p class="back">Kembali</p>
+    </div>
+    <p class="punya">Sudah mempunyai akun? <span style="color : #FFAB65" > Masuk</span></p>
+  </form>
+  <footer>
+  <p class="footer">© E-Culinary 2022. Hak Cipta Dilindungi</p>
+  </footer>  
+</body>
 </html>
 <?php
 if(isset($_POST["daftar"])){
