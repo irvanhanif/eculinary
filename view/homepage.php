4a70dfc4ebef20357ae853806a6042870785f716
irvanhanif24 <irhan8803@gmail.com>
2022-05-14 14:50:28 +0700

    add header in home

--- view/homepage.php
+++ view/homepage.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
-<meta charset="UTF-8">
+<head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="view/css/homepage.css">
-
+</head>
+<body>
+<?php require_once './view/header.html'; ?>
 <img class="banner" src="view/asset/banner.png" alt="banner">
 
 
@@ -120,5 +123,5 @@ for ($i = 0; $i <= 3; $i++) {
             }
             ?>  
 </div>
-
+</body>
 </html>
