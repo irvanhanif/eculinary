93ba5d7575c801bfc6e3a46e04c301cdb43e1614
ellenwidyaa <74695281+ellenwidyaa@users.noreply.github.com>
2022-05-16 01:53:21 +0700

    update Artikel

---
+++ view/artikel/artikel.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/artikel.css">
+    </head>
+    <body>
+        <div class="artikel-wrapper" >
+        <div class="artikel" onclick="location.href=''">
+            <img class="gambar-artikel" src="../asset/artikel.png" alt="artikel">
+            <h4 class="judul-artikel"><b>Judul Artikel</b></h4>
+                <h4 class="author">by Malang E-Culinary</h4>
+        </div>
+        </div>
+    </body>
+</html>
--- view/artikel/daftarArtikel.php
+++ view/artikel/daftarArtikel.php
@@ -1,10 +1,25 @@
-<?php require_once 'header.html'; ?>
-<p>Urutan Berdasarkan</p>
-<p>Terbaru</p>
-<div class="artikel">
-    <img src="artikel.png" alt="artikel">
-    <p>Segarnya Es Dempo Khas Malang Cocok
-Saat Cuaca Panas</p>
-    <p>by Malang e-culinary</p>
-</div>
-<?php require_once 'footer.html'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/daftarmakananminuman.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="filter-wrapper">
+            <h2>Urutkan Berdasarkan</h2>
+            <form action="">
+                <input type="button" name="terbaru" value="Terbaru">
+            </form>
+        </div>
+        <div>
+        <?php 
+            for ($i = 0; $i <= 2; $i++) {
+                require 'artikel.php';
+            }
+        ?>
+        </div>
+</body>
+
