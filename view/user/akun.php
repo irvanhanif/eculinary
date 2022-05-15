0b8d2d55014ebe332beca4f8ebb49bd8aafb9799
irvanhanif24 <irhan8803@gmail.com>
2022-05-15 20:47:10 +0700

    fix something

--- view/user/akun.php
+++ view/user/akun.php
@@ -6,59 +6,59 @@ if(isset($_SESSION["user-culinary"])){
 <html>
     <head>
         <link rel="stylesheet" href="../view/css/style.css">
-        <!-- <style>
+        <style>
             body{
                 margin: 0;
             }
             .wrapper{
-                margin-top: 150px;
+                margin-top: 100px;
             }
-        </style> -->
+        </style>
     </head>
     <body>
         <?php require_once 'view/header.php'; ?>
         <div class="wrapper">
             <?php include 'daftarKonten.php'; ?>
-                <form class="content" action="" method="post">
+                <form class="content" action="" method="post" enctype="multipart/form-data">
                     <div class="left-content">
                         <h1>Profil Saya</h1>
                         <div class="form">
                             <p>Username</p>
-                            <input id="username" type="text" name="username" placeholder="Masukan username Anda" required="required"><br>
+                            <input id="username" type="text" name="username" placeholder="Masukan username Anda" value = '<?php echo $data["username"] ?>' required><br>
                         </div>
                         <div class="form">
                             <p>Nama</p>
-                            <input id="nama" type="text" name="nama" placeholder="Masukan nama Anda" required="required"><br>
+                            <input id="nama" type="text" name="nama" placeholder="Masukan nama Anda" value = '<?php echo $data["nama"] ?>'><br>
                         </div>
                         <div class="form">
                             <p>Email</p>
-                            <input id="email" type="Email" name="email" placeholder="Masukan email Anda" required="required"><br>
+                            <input id="email" type="Email" name="email" placeholder="Masukan email Anda" value = '<?php echo $data["email"] ?>' required><br>
                         </div>
                         <div class="form">
                             <p>Nomor Telepon</p>
-                            <input id="telepon" type="text" name="telepon" placeholder="Masukan nomor telepon Anda" required="required"><br>
+                            <input id="telepon" type="text" name="telepon" placeholder="Masukan nomor telepon Anda" value = '<?php echo $data["nomor_telepon"] ?>'><br>
                         </div>
                         <div class="form">
                             <p>Alamat</p>
-                            <input id="alamat" type="text" name="alamat" placeholder="Masukan alamat Anda" required="required"><br>
+                            <input id="alamat" type="text" name="alamat" placeholder="Masukan alamat Anda" value = '<?php echo $data["alamat"] ?>'><br>
                         </div>
                         <div class="form">
                             <p>Jenis Kelamin</p>
-                            <input id="laki" type="radio" name="jenisKelamin">
+                            <input id="laki" type="radio" name="jenisKelamin" value="Laki-Laki" <?php if($data["jenis_kelamin"] == 'Laki-Laki') echo 'checked' ?>>
                             <label for="laki">Laki-laki</label>
-                            <input id="perempuan" type="radio" name="jenisKelamin">
+                            <input id="perempuan" type="radio" name="jenisKelamin" value="Perempuan" <?php if($data["jenis_kelamin"] == 'Perempuan') echo 'checked' ?>>
                             <label for="perempuan">Perempuan</label>
-                            <input id="lain" type="radio" name="jenisKelamin">
+                            <input id="lain" type="radio" name="jenisKelamin" value="Lainnya" <?php if($data["jenis_kelamin"] == 'Lainnya') echo 'checked' ?>>
                             <label for="lain">Lainnya</label><br>
                         </div>
                         <div class="form">
                             <p>Tanggal Lahir</p>
-                            <input id="tgl-lahir" type="date" name="tgl-lahir">
+                            <input id="tgl-lahir" type="date" name="tgl-lahir" value = '<?php echo $data["tanggal_lahir"] ?>'>
                         </div>
                         <input type="submit" name="submit" value="Simpan"> 
                     </div>
                     <div class="right-content">
-                        <img src="assets/imgs/photo-profile.png" alt="foto">
+                        <img src="../view/asset/profile.png" alt="foto">
                         <label class="pilih-gambar"><input type="file" accept=".jpg,.jpeg,.png"/>Pilih Gambar</label>
                     </div>     
                 </form>          
@@ -70,7 +70,7 @@ if(isset($_SESSION["user-culinary"])){
 <?php
     if(isset($_POST["submit"])){
         $user_1 = new c_user();
-        $user_1->updateAkun($_POST["username"], $_POST["nama"], $_POST["email"], $_POST["telepon"], $_POST["alamat"], $_POST["jeniskelamin"], $_POST["tanggallahir"]);
+        $user_1->updateAkun($_POST["username"], $_POST["nama"], $_POST["email"], $_POST["telepon"], $_POST["alamat"], $_POST["jenisKelamin"], $_POST["tgl-lahir"]);
     }
 }else{
       header("Location: register");
--- view/user/daftarKonten.php
+++ view/user/daftarKonten.php
@@ -5,34 +5,34 @@
     <body>
         <div class="list-content">
             <div class="profil">
-                <img src="assets/imgs/profile.png" alt="Profil">
+                <img src="../view/asset/profile.png" alt="Profil">
                 <div class="hi">
                     <p><b>Hi, Jong Un!</b></p>
                     <p>Kota Malang</p>
                 </div>
             </div>
             <div class="content-option">
-                <img src="assets/imgs/account.png" alt="akun">
+                <img src="../view/asset/account.png" alt="akun">
                 <a>Profil</a>
             </div>
             <div class="content-option">
-                <img src="assets/imgs/lock.png" alt="password">
+                <img src="../view/asset/lock.png" alt="password">
                 <a>Ubah Password</a>
             </div>
             <div class="content-option">
-                <img src="assets/imgs/favorite-file.png" alt="wishlist">
+                <img src="../view/asset/favorite-file.png" alt="wishlist">
                 <a>Wishlist</a>
             </div>
             <div class="content-option">
-                <img src="assets/imgs/shop.png" alt="toko">
+                <img src="../view/asset/shop.png" alt="toko">
                 <a>Toko</a>
             </div>
             <div class="content-option">
-                <img src="assets/imgs/edit-file.png" alt="tulisArtikel">
+                <img src="../view/asset/edit-file.png" alt="tulisArtikel">
                 <a>Tulis Artikel</a>
             </div>
             <div class="content-option">
-                <img src="assets/imgs/Logout.png" alt="logout">
+                <img src="../view/asset/Logout.png" alt="logout">
                 <a>Logout</a>
             </div>
         </div>
